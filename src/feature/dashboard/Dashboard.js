20110b1ea4120714ae7cafbc0d653937231331d9
francoisPgbn <francoispgbn@gmail.com>
2023-07-05 19:01:06 +0200

    added: dashboard layout

--- src/feature/dashboard/Dashboard.js
+++ src/feature/dashboard/Dashboard.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./Dashboard.scss";
 
 const Dashboard = () => {
   const { id } = useParams();
-  return <div>Hello world! = {id}</div>;
+  return (
+    <div className="layout-dashboard">
+      <div className="dashboard-content"></div>
+      <div className="dashboard-content"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
